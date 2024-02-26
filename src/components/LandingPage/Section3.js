eb03ee283b09e06e953f39460028c25a31b23614
MiriamPaternain <miriampaternain@gmail.com>
2024-02-26 22:37:36 +0100

    add structure and sytles section3

--- src/components/LandingPage/Section3.js
+++ src/components/LandingPage/Section3.js
@@ -1,5 +1,22 @@
 function Section3() {
-  return <></>;
+  return (
+    <>
+      <div className='section3Container'>
+        <a
+          className='section3Container_btn--link'
+          href='https://github.com/MiriamPaternain'>
+          {' '}
+          <button className='section3Container_btn'>Github</button>
+        </a>
+
+        <a
+          className='section3Container_btn--link'
+          href='https://www.behance.net/miriampaternin'>
+          <button className='section3Container_btn'>Behance </button>
+        </a>
+      </div>
+    </>
+  );
 }
 
 export default Section3;
