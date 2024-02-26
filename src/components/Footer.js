a2ff1e03a93360ebdf58cac72c3d69354ed3ebca
MiriamPaternain <miriampaternain@gmail.com>
2024-02-26 14:38:39 +0100

    footer style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,22 @@
 import { FaRegHeart } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 function Footer() {
   return (
     <>
       <div className='footerContainer'>
         <ul className='footerContainer_list'>
-          <li className='footerContainer_list--link'>Home</li>
-          <li className='footerContainer_list--link'>About</li>
-          <li className='footerContainer_list--link'>Projects</li>
-          <li className='footerContainer_list--link'>Contact</li>
+          <Link to='/' className='footerContainer_list--link'>
+            <li>Home</li>
+          </Link>
+          <Link className='footerContainer_list--link' to='/About'>
+            <li>About</li>
+          </Link>
+          <Link className='footerContainer_list--link' to='/Projects'>
+            <li>Projects</li>
+          </Link>
+          <Link className='footerContainer_list--link' to='/Contact'>
+            <li>Contact</li>
+          </Link>
         </ul>
         <p className='footerContainer_phrase'>Thanks for watching!</p>
         <p className='footerContainer_phrase'>
