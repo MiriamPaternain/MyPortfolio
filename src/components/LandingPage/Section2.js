542dd65f00109f92ebd84ccdf6181a8c45eb9439
MiriamPaternain <miriampaternain@gmail.com>
2024-02-26 22:20:26 +0100

    add section2, structure en style

--- src/components/LandingPage/Section2.js
+++ src/components/LandingPage/Section2.js
@@ -1,5 +1,14 @@
 function Section2() {
-  return <></>;
+  return (
+    <>
+      <section className='section2Container'>
+        <p className='section2Container_phrase'>
+          Hi, I'm Miriam Paternáin, a passionate designer and web developer
+          based in Madrid.
+        </p>
+      </section>
+    </>
+  );
 }
 
 export default Section2;
