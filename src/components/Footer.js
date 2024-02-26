df146f50853df625691b08b2fdc8e011fe77ea71
MiriamPaternain <miriampaternain@gmail.com>
2024-02-26 14:18:20 +0100

    add structure to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,22 @@
+import { FaRegHeart } from 'react-icons/fa';
 function Footer() {
-  return <></>;
+  return (
+    <>
+      <div className='footerContainer'>
+        <ul className='footerContainer_list'>
+          <li className='footerContainer_list--link'>Home</li>
+          <li className='footerContainer_list--link'>About</li>
+          <li className='footerContainer_list--link'>Projects</li>
+          <li className='footerContainer_list--link'>Contact</li>
+        </ul>
+        <p className='footerContainer_phrase'>Thanks for watching!</p>
+        <p className='footerContainer_phrase'>
+          Designed and developed by Miriam Paternáin{' '}
+          <FaRegHeart className='footerContainer_phrase--heartIcon' /> 2024
+        </p>
+      </div>
+    </>
+  );
 }
 
 export default Footer;
