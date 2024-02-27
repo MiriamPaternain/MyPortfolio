7fec8b5ceb0fc30f79e1eb75fddb881080beca85
MiriamPaternain <miriampaternain@gmail.com>
2024-02-27 09:48:15 +0100

    add section my projects structure

--- src/components/LandingPage/SectionMyProjects.js
+++ src/components/LandingPage/SectionMyProjects.js
@@ -1,5 +1,46 @@
+import MyStore from '../../img/My-store.png';
+import GreenCoffe from '../../img/greenCoffe.png';
+import DoLister from '../../img/Do Lister.png';
 function SectionMyProjects() {
-  return <></>;
+  const projects = [
+    {
+      img: { MyStore },
+      name: 'My Store',
+      info: 'Web Development',
+    },
+    {
+      img: { GreenCoffe },
+      name: 'The Green Coffe',
+      info: 'Brand Identity',
+    },
+    {
+      img: { DoLister },
+      name: 'Do Lister',
+      info: 'Web Development',
+    },
+  ];
+  return (
+    <>
+      <main className='sectionMyProjectsContainer'>
+        <title className='sectionMyProjectsContainer_title'>My PROJECTS</title>
+        <article>
+          <img
+            src={projects.img}
+            alt=''
+            className='sectionMyProjectsContainer_img'
+          />
+          <div className='sectionMyProjectsContainer_infoBlock'>
+            <h1 className='sectionMyProjectsContainer_infoBlock--name'>
+              {projects.name}
+            </h1>
+            <h3 className='sectionMyProjectsContainer_infoBlock--info'>
+              {projects.info}
+            </h3>
+          </div>
+        </article>
+      </main>
+    </>
+  );
 }
 
 export default SectionMyProjects;
