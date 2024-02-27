7e29925b928869f15cef047b63161308d0e1f132
MiriamPaternain <miriampaternain@gmail.com>
2024-02-27 15:10:32 +0100

    add javascript in contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Footer from './Footer';
 import Nav from './Nav';
 import { FiMapPin } from 'react-icons/fi';
@@ -5,6 +6,18 @@ import { GoMail } from 'react-icons/go';
 import { LuLinkedin } from 'react-icons/lu';
 
 function Contact() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [issue, setIssue] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setName('');
+    setEmail('');
+    setIssue('');
+    setMessage('');
+  };
   return (
     <>
       <Nav />
@@ -28,15 +41,22 @@ function Contact() {
           </a>
         </div>
         <p className='contactContainer_formTitle'>Contact Form</p>
-        <form action='' className='contactContainer_form'>
+        <form
+          onSubmit={handleSubmit}
+          action='https://formsubmit.co/miriampaternain@gmail.com'
+          method='POST'
+          className='contactContainer_form'>
           <label htmlFor='name' className='contactContainer_form--label'>
             Complete name
           </label>
           <input
             type='text'
             id='name'
+            name='Name'
             placeholder='Miriam Paternáin'
             className='contactContainer_form--input'
+            value={name}
+            onChange={(event) => setName(event.target.value)}
           />
           <label htmlFor='email' className='contactContainer_form--label'>
             Your email
@@ -44,8 +64,11 @@ function Contact() {
           <input
             type='text'
             id='email'
+            name='Email'
             placeholder='example@example.com'
             className='contactContainer_form--input'
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
           />
           <label htmlFor='issue' className='contactContainer_form--label'>
             Issue
@@ -53,8 +76,11 @@ function Contact() {
           <input
             type='text'
             id='issue'
+            name='Issue'
             placeholder='Hello!'
             className='contactContainer_form--input'
+            value={issue}
+            onChange={(event) => setIssue(event.target.value)}
           />
           <label htmlFor='message' className='contactContainer_form--label'>
             Message
@@ -62,10 +88,15 @@ function Contact() {
           <input
             type='text'
             id='message'
+            name='Message'
             placeholder='Write your message here'
             className='contactContainer_form--inputMessage'
+            value={message}
+            onChange={(event) => setMessage(event.target.value)}
           />
-          <button className='contactContainer_form--btn'>Send Message</button>
+          <button className='contactContainer_form--btn' type='submit'>
+            Send Message
+          </button>
         </form>
       </div>
       <Footer />
