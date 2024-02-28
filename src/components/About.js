65fad09b4d5badf52a2db564332999f2132c7190
MiriamPaternain <miriampaternain@gmail.com>
2024-02-28 11:17:44 +0100

    add skills styles

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,18 @@
 import Footer from './Footer';
 import Nav from './Nav';
 import introImg from '../img/yo.JPG';
+import illustrator from '../img/ilustrador.png';
+import photoshop from '../img/photoshop.png';
+import indesign from '../img/indesign.png';
+import figma from '../img/figma.png';
+import uxUi from '../img/ux:ui.png';
+import html from '../img/html-5.png';
+import css from '../img/css-3.png';
+import js from '../img/js.png';
+import react from '../img/react.png';
+import node from '../img/node.png';
+import git from '../img/git.png';
+import terminal from '../img/terminal.png';
 
 function About() {
   return (
@@ -16,22 +28,23 @@ function About() {
           </p>
         </section>
         <section className='aboutContainer_skills'>
+          <div className='aboutContainer_skills--line'></div>
           <p className='aboutContainer_skills--text'>Skills</p>
-          <div className='aboutContainer_skills-designIcons'>
-            <img src='' alt='' className='' />
-            <img src='' alt='' className='' />
-            <img src='' alt='' className='' />
-            <img src='' alt='' className='' />
-            <img src='' alt='' className='' />
+          <div className='aboutContainer_skills--designIcons'>
+            <img className='icon' src={illustrator} alt='illustrator icon' />
+            <img src={photoshop} alt='photoshop icon' className='icon' />
+            <img src={indesign} alt='indesign icon' className='icon' />
+            <img src={figma} alt='figma icon' className='icon' />
+            <img src={uxUi} alt='uxui icon' className='icon' />
           </div>
-          <div className='aboutContainer_skills-developmentIcons'>
-            <img src='' alt='' className='' />
-            <img src='' alt='' className='' />
-            <img src='' alt='' className='' />
-            <img src='' alt='' className='' />
-            <img src='' alt='' className='' />
-            <img src='' alt='' className='' />
-            <img src='' alt='' className='' />
+          <div className='aboutContainer_skills--developmentIcons'>
+            <img src={html} alt='html icon' className='icon' />
+            <img src={css} alt='css icon' className='icon' />
+            <img src={js} alt='javascript icon' className='icon' />
+            <img src={react} alt='react icon' className='icon' />
+            <img src={node} alt='node js icon' className='icon' />
+            <img src={git} alt='git icon' className='icon' />
+            <img src={terminal} alt='terminal icon' className='icon' />
           </div>
         </section>
         <section className='aboutContainer_softSkills'>
