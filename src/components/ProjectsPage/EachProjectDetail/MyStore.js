4d89ec2d94dbe3ae4ce329907dfa081e912ee22e
MiriamPaternain <miriampaternain@gmail.com>
2024-02-29 11:00:42 +0100

    add under construction img

--- src/components/ProjectsPage/EachProjectDetail/MyStore.js
+++ src/components/ProjectsPage/EachProjectDetail/MyStore.js
@@ -1,10 +1,12 @@
 import Footer from '../../Footer';
 import Nav from '../../Nav';
+import underconstruction from '../../../img/underConstruction.jpg';
 
 function MyStore() {
   return (
     <>
       <Nav />
+      <img src={underconstruction} alt='' className='underConstrucion' />
       <Footer />
     </>
   );
