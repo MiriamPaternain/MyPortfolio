c67800ea6c0316aec64df58aaa816c8bb81ab48a
MiriamPaternain <miriampaternain@gmail.com>
2024-02-29 13:24:18 +0100

    fix aligment in 1024

--- src/components/ProjectsPage/Carousel.js
+++ src/components/ProjectsPage/Carousel.js
@@ -18,6 +18,7 @@ const Carousel = () => {
   const settings = {
     dots: true,
     infinite: true,
+    arrows: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
--- src/components/ProjectsPage/Projects.js
+++ src/components/ProjectsPage/Projects.js
@@ -6,6 +6,7 @@ function Projects() {
   return (
     <>
       <Nav />
+
       <Carousel />
       <Footer />
     </>
