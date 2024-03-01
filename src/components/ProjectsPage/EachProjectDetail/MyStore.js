76412cdf0844985c47d229aa336219f9adbe6bc0
MiriamPaternain <miriampaternain@gmail.com>
2024-03-01 09:35:05 +0100

    add each project html structure

--- src/components/ProjectsPage/EachProjectDetail/MyStore.js
+++ src/components/ProjectsPage/EachProjectDetail/MyStore.js
@@ -6,7 +6,16 @@ function MyStore() {
   return (
     <>
       <Nav />
-      <img src={underconstruction} alt='' className='underConstrucion' />
+      <main className='myStoreContainer'>
+        <p className='myStoreContainer_title'></p>
+        <p className='myStoreContainer_resume'></p>
+        <div className='myStoreContainer_skills'></div>
+        <button className='myStoreContainer_btn'></button>
+        <button className='myStoreContainer_btn'></button>
+        <img src='' alt='' className='myStoreContainer_img' />
+        <img src='' alt='' className='myStoreContainer_img' />
+        <img src='' alt='' className='myStoreContainer_img' />
+      </main>
       <Footer />
     </>
   );
