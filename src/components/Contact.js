9dcdef32dc8b2d76a0730024b8113efc7a65698d
MiriamPaternain <miriampaternain@gmail.com>
2024-03-05 14:16:20 +0100

    change name public key

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -16,11 +16,11 @@ function Contact() {
 
     const serviceId = process.env.REACT_APP_SERVICE_ID;
     const templateId = process.env.REACT_APP_TEMPLATE_ID;
-    const publicKey = process.env.REACT_APP_PUBLIC_KEY;
+    const publicKeyEmail = process.env.REACT_APP_PUBLIC_KEY;
 
     emailjs
       .sendForm(serviceId, templateId, form.current, {
-        publicKey: publicKey,
+        publicKey: publicKeyEmail,
       })
       .then(
         () => {
