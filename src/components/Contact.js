f394496804ee94c39f70cad62fa0477d8219f7a9
MiriamPaternain <miriampaternain@gmail.com>
2024-03-05 13:31:58 +0100

    add emailjs

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,37 +1,34 @@
-import { useState } from 'react';
-import axios from 'axios';
+import { useRef } from 'react';
+
 import Footer from './Footer';
 import Nav from './Nav';
 import { FiMapPin } from 'react-icons/fi';
 import { GoMail } from 'react-icons/go';
 import { LuLinkedin } from 'react-icons/lu';
+import emailjs from '@emailjs/browser';
 
 function Contact() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [subject, setSubject] = useState('');
-  const [message, setMessage] = useState('');
+  const form = useRef();
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    const formData = new FormData();
-    formData.append('name', name);
-    formData.append('email', email);
-    formData.append('subject', subject);
-    formData.append('message', message);
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-    try {
-      await axios.post(
-        'https://formsubmit.co/miriampaternain@gmail.com',
-        formData
+    const serviceId = process.env.REACT_APP_SERVICE_ID;
+    const templateId = process.env.REACT_APP_TEMPLATE_ID;
+    const publicKey = process.env.REACT_APP_PUBLIC_KEY;
+
+    emailjs
+      .sendForm(serviceId, templateId, form.current, {
+        publicKey: publicKey,
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        }
       );
-      setName('');
-      setEmail('');
-      setSubject('');
-      setMessage('');
-    } catch (error) {
-      console.error('Error sending the form:', error);
-    }
   };
   return (
     <>
@@ -56,21 +53,16 @@ function Contact() {
           </a>
         </div>
         <p className='contactContainer_formTitle'>Contact Form</p>
-        <form
-          onSubmit={handleSubmit}
-          action={`https://formsubmit.co/${process.env.REACT_APP_FORMSUBMIT_ID}`}
-          className='contactContainer_form'>
+        <form ref={form} onSubmit={sendEmail} className='contactContainer_form'>
           <label htmlFor='name' className='contactContainer_form--label'>
             Complete name / Nombre completo
           </label>
           <input
             type='text'
             id='name'
-            name='name'
+            name='user_name'
             placeholder='Name last Name / Nombre Apellido'
             className='contactContainer_form--input'
-            value={name}
-            onChange={(event) => setName(event.target.value)}
           />
           <label htmlFor='email' className='contactContainer_form--label'>
             Your email / Email
@@ -81,8 +73,6 @@ function Contact() {
             name='email'
             placeholder='example@example.com'
             className='contactContainer_form--input'
-            value={email}
-            onChange={(event) => setEmail(event.target.value)}
           />
           <label htmlFor='subject' className='contactContainer_form--label'>
             Subject / Asunto
@@ -93,8 +83,6 @@ function Contact() {
             name='subject'
             placeholder='Hello!'
             className='contactContainer_form--input'
-            value={subject}
-            onChange={(event) => setSubject(event.target.value)}
           />
           <label htmlFor='message' className='contactContainer_form--label'>
             Message / Mensaje
@@ -102,11 +90,9 @@ function Contact() {
           <textarea
             type='text'
             id='message'
-            name='Message'
+            name='message'
             placeholder='Write your message here'
             className='contactContainer_form--inputMessage'
-            value={message}
-            onChange={(event) => setMessage(event.target.value)}
           />
           <button className='contactContainer_form--btn' type='submit'>
             Send / Enviar
