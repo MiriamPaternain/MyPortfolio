9a29f2d1f75cadf3fe89b77d41935272c1f4122b
MiriamPaternain <miriampaternain@gmail.com>
2024-03-06 13:49:19 +0100

    fix footer in projects

--- src/components/ProjectsPage/Projects.js
+++ src/components/ProjectsPage/Projects.js
@@ -1,13 +1,40 @@
 import Carousel from './Carousel';
 import Footer from '../Footer';
 import Nav from '../Nav';
+import { useEffect, useState } from 'react';
 
 function Projects() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+    return () => clearTimeout(timeout);
+  }, []);
   return (
     <>
-      <Nav />
-      <Carousel />
-      <Footer />
+      {loading ? (
+        <div className='loader'>
+          {/*  <img src={backgroundLoader} alt='loading...' className='loader_img' /> */}
+          <div class='lds-roller'>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+      ) : (
+        <>
+          <Nav />
+          <Carousel />
+          <Footer />
+        </>
+      )}
     </>
   );
 }
