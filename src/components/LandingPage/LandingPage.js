c895471fbd3c8c83ecf8e0581661dcf95aa3113f
MiriamPaternain <miriampaternain@gmail.com>
2024-03-06 10:36:16 +0100

    add loading to landing

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -4,18 +4,44 @@ import Section3 from './Section3';
 import SectionMyProjects from './SectionMyProjects';
 import Nav from '../Nav';
 import Footer from '../Footer';
+import backgroundLoader from '../../img/fondo loading.png';
+import { useEffect, useState } from 'react';
 
 function LandingPage() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+    return () => clearTimeout(timeout);
+  }, []);
   return (
     <>
-      <main className='landingPage'>
-        <Nav />
-        <Section1 />
-        <Section2 />
-        <Section3 />
-        <SectionMyProjects />
-        <Footer />
-      </main>
+      {loading ? (
+        <div className='loader'>
+          <img src={backgroundLoader} alt='loading...' className='loader_img' />
+          <div class='lds-roller'>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+      ) : (
+        <main className='landingPage'>
+          <Nav />
+          <Section1 />
+          <Section2 />
+          <Section3 />
+          <SectionMyProjects />
+          <Footer />
+        </main>
+      )}
     </>
   );
 }
