4247c1e31c694dc75625d65929cf098ab77dde44
MiriamPaternain <miriampaternain@gmail.com>
2024-03-10 19:07:30 +0100

    change add item btn position

--- src/components/ProjectsPage/EachProjectDetail/DoLister.js
+++ src/components/ProjectsPage/EachProjectDetail/DoLister.js
@@ -1,12 +1,68 @@
 import Footer from '../../Footer';
 import Nav from '../../Nav';
-import underconstruction from '../../../img/underConstruction.jpg';
+import html from '../../../img/html-5.png';
+import css from '../../../img/css-3.png';
+import js from '../../../img/js.png';
+import react from '../../../img/react.png';
+import node from '../../../img/node.png';
+import git from '../../../img/git.png';
+import terminal from '../../../img/terminal.png';
+import { FaGithub } from 'react-icons/fa';
+import { TfiWorld } from 'react-icons/tfi';
+import productDetail from '../../../img/productdetail.png';
+import shoppinCart from '../../../img/shoppingcart.png';
+import products from '../../../img/products.png';
 
 function DoLister() {
   return (
     <>
       <Nav />
-      <img src={underconstruction} alt='' className='underConstrucion' />
+      <main className='myStoreContainer'>
+        <p className='myStoreContainer_title'>My Store</p>
+        <p className='myStoreContainer_resume'>
+          DoLister es una aplicación web para realizar listas. Cada elemento
+          añadido se puede tachar una vez que ya se ha realizado y también se
+          puede borrar. Ademas cuenta con un buscador para encontrar antes la
+          tarea que se está buscando.
+        </p>
+        <p className='myStoreContainer_skillsTitle'>Skills:</p>
+        <div className='myStoreContainer_skills'>
+          <img src={html} alt='' className='myStoreContainer_skills--icon' />
+          <img src={css} alt='' className='myStoreContainer_skills--icon' />
+          <img src={js} alt='' className='myStoreContainer_skills--icon' />
+          <img src={react} alt='' className='myStoreContainer_skills--icon' />
+          <img src={node} alt='' className='myStoreContainer_skills--icon' />
+          <img src={git} alt='' className='myStoreContainer_skills--icon' />
+          <img
+            src={terminal}
+            alt=''
+            className='myStoreContainer_skills--icon'
+          />
+        </div>
+
+        <a
+          href='https://github.com/MiriamPaternain/DoLister'
+          target='_blank'
+          rel='noreferrer'
+          className='myStoreContainer_link'>
+          <button className='myStoreContainer_link--btn'>
+            <FaGithub className='myStoreContainer_link--icon' /> Github
+          </button>
+        </a>
+        <a
+          href='miriampaternain.github.io/DoLister/'
+          target='_blank'
+          rel='noreferrer'
+          className='myStoreContainer_link'>
+          <button className='myStoreContainer_link--btn'>
+            <TfiWorld className='myStoreContainer_link--icon' /> Link to web
+          </button>
+        </a>
+        <img src={productDetail} alt='' className='myStoreContainer_img' />
+        <img src={shoppinCart} alt='' className='myStoreContainer_img' />
+        <img src={products} alt='' className='myStoreContainer_img' />
+        <img src='' alt='' className='myStoreContainer_img' />
+      </main>
       <Footer />
     </>
   );
